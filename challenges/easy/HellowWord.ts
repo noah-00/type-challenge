3b1c196ea728226c1d46766a79e1497d8e3ea9f0
noah-00 <t71.ishizaka.naoya@gmail.com>
2023-10-30 08:50:05 +0900

    01 Hello word

---
+++ challenges/easy/HellowWord.ts
@@ -0,0 +1,7 @@
+import { Equal, Expect } from "../../utils/index";
+
+// expected to be string
+type HelloWorld = string;
+
+// you should make this work
+type test = Expect<Equal<HelloWorld, string>>;
