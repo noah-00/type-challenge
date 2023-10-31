f226c42d98304a56de7a58dd05597af6b7c9e95a
noah-00 <t71.ishizaka.naoya@gmail.com>
2023-10-31 09:00:28 +0900

    ✨ feat: practice readonly

--- challenges/easy/03_Readonly.ts
+++ challenges/easy/03_Readonly.ts
@@ -20,3 +20,22 @@ const todo: MyReadonly<Todo> = {
 todo.title = "Hello"; // Error: cannot reassign a readonly property
 // @ts-expect-error
 todo.description = "barFoo"; // Error: cannot reassign a readonly property
+
+interface User {
+  name: string;
+  id: number;
+}
+
+type MyReadonly2<T> = {
+  readonly [P in keyof T]: T[P];
+};
+
+const user: MyReadonly2<User> = {
+  name: "noah",
+  id: 2,
+};
+
+// @ts-expect-error
+user.name = "Hello"; // Error: cannot reassign a readonly property
+// @ts-expect-error
+user.id = 1; // Error: cannot reassign a readonly property
