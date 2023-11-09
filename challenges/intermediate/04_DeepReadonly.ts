6c96cfdd6429d56691ce24a8d5fdfb74bb5bbf12
noah-00 <t71.ishizaka.naoya@gmail.com>
2023-11-10 08:47:04 +0900

    ✨ 04 DeepReadonly

---
+++ challenges/intermediate/04_DeepReadonly.ts
@@ -0,0 +1,22 @@
+type X = {
+  x: {
+    a: 1;
+    b: "hi";
+  };
+  y: "hey";
+};
+
+type Expected = {
+  readonly x: {
+    readonly a: 1;
+    readonly b: "hi";
+  };
+  readonly y: "hey";
+};
+
+type DeepReadonly<T> = keyof T extends never ? T : { readonly [k in keyof T]: DeepReadonly<T[k]> };
+
+type Todo = DeepReadonly<X>; // should be same as `Expected`
+
+type Primitive = string | number | boolean;
+type PrimitiveResult = DeepReadonly<Primitive>;
