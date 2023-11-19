44c603de7824eea1fb8dafa71c9ab42eeea8cb1f
noah-00 <t71.ishizaka.naoya@gmail.com>
2023-11-19 11:35:42 +0900

    ✨ 14 ReplaceAll

---
+++ challenges/intermediate/14_ReplaceAll.ts
@@ -0,0 +1,7 @@
+type ReplaceAll<S extends string, From extends string, To extends string> = From extends ""
+  ? S
+  : S extends `${infer R1}${From}${infer R2}`
+  ? `${R1}${To}${ReplaceAll<R2, From, To>}`
+  : S;
+
+type replaced = ReplaceAll<"t y p e s", " ", "">; // expected to be 'types'
