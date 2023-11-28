029b11f88dfd65fe0305950e9997b62f3f7ec9fd
noah-00 <t71.ishizaka.naoya@gmail.com>
2023-11-28 21:23:53 +0900

    ✨ 23_ KebabCase

---
+++ challenges/intermediate/23_ KebabCase.ts
@@ -0,0 +1,11 @@
+type KebabCase<S extends string> = S extends `${infer S1}${infer S2}`
+  ? S2 extends Uncapitalize<S2>
+    ? `${Uncapitalize<S1>}${KebabCase<S2>}`
+    : `${Uncapitalize<S1>}-${KebabCase<S2>}`
+  : S;
+
+type FooBarBaz = KebabCase<"FooBarBaz">;
+const foobarbaz: FooBarBaz = "foo-bar-baz";
+
+type DoNothing = KebabCase<"do-nothing">;
+const doNothing: DoNothing = "do-nothing";
