901d441d1b416ca6649b80e087807095eef98981
noah-00 <t71.ishizaka.naoya@gmail.com>
2023-11-29 20:21:46 +0900

    ✨ 24_anyoff

---
+++ challenges/intermediate/24_anyoff.ts
@@ -0,0 +1,6 @@
+type AnyOf<T extends any[]> = T[number] extends 0 | "" | false | [] | { [key: string]: never }
+  ? false
+  : true;
+
+type Sample1 = AnyOf<[1, "", false, [], {}]>; // expected to be true.
+type Sample2 = AnyOf<[0, "", false, [], {}]>; // expected to be false.
