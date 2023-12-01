8367fcac0b1b8ec05497328f29875d8a84e4252b
noah-00 <t71.ishizaka.naoya@gmail.com>
2023-12-01 20:50:35 +0900

    ✨ 26_isUnion

---
+++ challenges/intermediate/26_isUnion.ts
@@ -0,0 +1,5 @@
+type IsUnion<T, B = T> = T extends B ? ([B] extends [T] ? false : true) : never;
+
+type case1 = IsUnion<string>; // false
+type case2 = IsUnion<string | number>; // true
+type case3 = IsUnion<[string | number]>; // false
