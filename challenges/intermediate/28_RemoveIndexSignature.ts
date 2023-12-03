8d46ec0e584e0afa01a4787f3666c754d2381712
noah-00 <t71.ishizaka.naoya@gmail.com>
2023-12-03 21:45:28 +0900

    ✨ 28_RemoveIndexSignature

---
+++ challenges/intermediate/28_RemoveIndexSignature.ts
@@ -0,0 +1,10 @@
+type Foo = {
+  [key: string]: any;
+  foo(): void;
+};
+
+type RemoveIndexSignature<T> = {
+  [K in keyof T as string extends K ? never : number extends K ? never : K]: T[K];
+};
+
+type A = RemoveIndexSignature<Foo>; // expected { foo(): void }
