b4bfdac29bc593e8103a999334cd476fad76b4ec
noah-00 <t71.ishizaka.naoya@gmail.com>
2023-12-06 08:44:41 +0900

    31_ MinusOne

---
+++ challenges/intermediate/31_ MinusOne.ts
@@ -0,0 +1,8 @@
+type Pop<T extends any[]> = T extends [...infer head, any] ? head : never;
+
+type MinusOne<T extends number, A extends any[] = []> = A["length"] extends T
+  ? Pop<A>["length"]
+  : MinusOne<T, [...A, 0]>;
+
+type Zero = MinusOne<1>; // 0
+type FiftyFour = MinusOne<55>; // 54
