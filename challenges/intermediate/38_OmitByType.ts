3fbe99afe1060df597f7c581800ab1200d0641c4
noah-00 <t71.ishizaka.naoya@gmail.com>
2023-12-11 08:38:15 +0900

    38_OmitByType

---
+++ challenges/intermediate/38_OmitByType.ts
@@ -0,0 +1,13 @@
+type OmitByType<T, A> = {
+  [P in keyof T as T[P] extends A ? never : P]: T[P];
+};
+
+type OmitBoolean = OmitByType<
+  {
+    name: string;
+    count: number;
+    isReadonly: boolean;
+    isEnable: boolean;
+  },
+  boolean
+>; // { name: string; count: number }
