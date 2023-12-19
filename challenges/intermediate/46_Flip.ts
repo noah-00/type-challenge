266b39d1ee6bb993919f504c35a08a1cde1e8ee2
noah-00 <t71.ishizaka.naoya@gmail.com>
2023-12-19 19:56:02 +0900

    46_Flip

---
+++ challenges/intermediate/46_Flip.ts
@@ -0,0 +1,7 @@
+type Flip<T extends Record<PropertyKey, any>> = {
+  [P in keyof T as `${T[P]}`]: P;
+};
+
+type a = Flip<{ a: "x"; b: "y"; c: "z" }>; // {x: 'a', y: 'b', z: 'c'}
+type b = Flip<{ a: 1; b: 2; c: 3 }>; // {1: 'a', 2: 'b', 3: 'c'}
+type c = Flip<{ a: false; b: true }>; // {false: 'a', true: 'b'}
