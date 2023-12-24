20911cf3b0643f1ffd8658b17447a169af10b6fa
noah-00 <t71.ishizaka.naoya@gmail.com>
2023-12-24 09:27:27 +0900

    51_IsTuple

---
+++ challenges/intermediate/51_IsTuple.ts
@@ -0,0 +1,5 @@
+type IsTuple<T> = T extends readonly any[] ? (number extends T["length"] ? false : true) : false;
+
+type case1 = IsTuple<[number]>; // true
+type case2 = IsTuple<readonly [number]>; // true
+type case3 = IsTuple<number[]>; // false
