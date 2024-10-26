95c6fbdcc8d322ff998dd7493d4727f76088aa93
noah-00 <t71.ishizaka.naoya@gmail.com>
2024-10-26 10:41:17 -0700

    07_Awaited_3

---
+++ challenges/easy_2/07_Awaited_3.ts
@@ -0,0 +1,9 @@
+type ExampleType = Promise<string>;
+
+type MyAwaited<T extends PromiseLike<any>> = T extends PromiseLike<infer U>
+  ? U extends PromiseLike<any>
+    ? MyAwaited<U>
+    : U
+  : never;
+
+type Result = MyAwaited<ExampleType>; // string
